61a9af596b53f2d13dcfbbfc2912c5124f6cdca4
ETUC <127153313+etuclamba@users.noreply.github.com>
2023-04-06 23:24:17 +0100

    Update list.py

--- list.py
+++ list.py
@@ -1,10 +1,3 @@
-def has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10):
-
-    for element in list1:
-        if element in list2 or element in list3 or element in list4 or element in list5 or element in list6 or element in list7 or element in list8 or element in list9 or element in list10:
-            return True
-    return False
-
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
 list3 = [7, 1, 9]
@@ -16,10 +9,19 @@ list8 = [22, 23, 24]
 list9 = [25, 26, 27]
 list10 = [28, 29, 30]
 
-if has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10):
+
+def has_common_member():
+
+    for element in list1:
+        if element in list2 or element in list3 or element in list4 or element in list5 or element in list6 or element in list7 or element in list8 or element in list9 or element in list10:
+            return True
+    return False
+
+
+if has_common_member():
     print("TRUE")
 else:
     print("FALSE")
 
 
-has_common_member(list1, list2, list3, list4, list5, list6, list7, list8, list9, list10)
+has_common_member()
